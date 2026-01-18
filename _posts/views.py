fae601ee058dc38e57ad1f051ecb824982990a5c
Ashrafulx <ashrafulwho@gmail.com>
2026-01-18 21:43:04 +0100

    feat: implement HTMX navigation with partial views and dynamic content loading

--- _posts/views.py
+++ _posts/views.py
@@ -4,16 +4,23 @@ def home(request):
     context = {
         'page' : 'Home',
     }
+    if request.htmx:
+        return render(request, '_posts/partials/_home.html', context)
     return render(request, '_posts/home.html', context)
 
 def explore(request):
     context = {
         'page' : 'Explore',
     }
+    if request.htmx:
+        return render(request, '_posts/partials/_explore.html', context)
     return render(request, '_posts/explore.html', context)
 
 def upload(request):
     context = {
         'page' : 'Upload',
     }
+    
+    if request.htmx:
+        return render(request, '_posts/partials/_upload.html', context)
     return render(request, '_posts/upload.html', context)
