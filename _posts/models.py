3984b93ec651d3ac024eb1009a683360204e24a8
Ashrafulx <ashrafulwho@gmail.com>
2026-01-21 03:06:43 +0100

    feat: add Post model with UUID and author relationship

--- _posts/models.py
+++ _posts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
+import uuid
 
-# Create your models here.
+class Post(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='posts'
+    )
+    image = models.ImageField(upload_to='posts/')
+    body = models.CharField(max_length=80, null=True, blank=True)
+    tags = models.CharField(max_length=80, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.uuid)
