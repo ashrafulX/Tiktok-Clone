c85f5fa04251d8cc77f33c1f670c91229c9d22d7
Ashrafulx <ashrafulwho@gmail.com>
2026-01-21 03:08:20 +0100

    feat: render posts

--- _posts/views.py
+++ _posts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 def home(request):
+    posts = Post.objects.all()
     context = {
         'page' : 'Home',
+        'posts': posts
     }
     if request.htmx:
         return render(request, '_posts/partials/_home.html', context)
