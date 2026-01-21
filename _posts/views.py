d12a42f42d80ef266b2b1337c663122f8d2e5121
Ashrafulx <ashrafulwho@gmail.com>
2026-01-22 00:36:12 +0100

    feat: add infinite scroll pagination to home feed with htmx

--- _posts/views.py
+++ _posts/views.py
@@ -2,13 +2,27 @@ from django.shortcuts import render, redirect
 from .forms import PostForm
 from .models import Post
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 def home(request):
-    posts = Post.objects.all()
+    posts = Post.objects.order_by('-created_at')
+    
+    paginator = Paginator(posts, 1)
+    page_number = int(request.GET.get('page_number', 1))
+    posts_page = paginator.get_page(page_number)
+    next_page = posts_page.next_page_number() if posts_page.has_next() else None
+    page_start_index = (posts_page.number - 1 ) * paginator.per_page
+    
     context = {
         'page' : 'Home',
-        'posts': posts
+        'posts': posts_page,
+        'next_page': next_page,
+        'page_start_index': page_start_index
     }
+    
+    if request.GET.get('paginator'):
+        return render(request, '_posts/partials/_posts.html', context)
+    
     if request.htmx:
         return render(request, '_posts/partials/_home.html', context)
     return render(request, '_posts/home.html', context)
