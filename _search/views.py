cf7538489ca0561902289766c85af1a552199644
Ashrafulx <ashrafulwho@gmail.com>
2026-05-12 00:12:29 +0100

    feat: Add tag suggestions to search dropdown

--- _search/views.py
+++ _search/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from django.db.models import Q, Count
-from _posts.models import Post
+from _posts.models import Post, Tag
 
 User = get_user_model()
 
@@ -40,6 +40,7 @@ def search_suggestions(request):
     query = request.GET.get('q', None)
     
     user_suggestions = User.objects.none()
+    tag_suggestions = User.objects.none()
     
     if query and len(query) > 2:
         user_suggestions = User.objects.filter(
@@ -47,9 +48,12 @@ def search_suggestions(request):
             Q(name__icontains=query) |
             Q(bio__icontains=query)
         ).annotate(followers_count=Count('is_followed', distinct=True)).order_by('-followers_count')[:5]
+        
+        tag_suggestions = Tag.objects.filter(name__istartswith=query)[:5]
     
     context = {
-        'user_suggestions': user_suggestions
+        'user_suggestions': user_suggestions,
+        'tag_suggestions': tag_suggestions,
     }
     
     return render(request, '_search/partials/_search_suggestions.html', context)
