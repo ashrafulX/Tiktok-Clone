63737cc4122618d2ebce0de42b1a7b2489c2ac8a
Ashrafulx <ashrafulwho@gmail.com>
2026-05-16 16:13:08 +0100

    fix: Prevent back-button access to authenticated pages after logout

--- _core/settings.py
+++ _core/settings.py
@@ -68,7 +68,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "django_htmx.middleware.HtmxMiddleware",
-    "allauth.account.middleware.AccountMiddleware"
+    "allauth.account.middleware.AccountMiddleware",
+    "_core.middleware.NoCacheForAuthenticatedMiddleware",
 ]
 
 AUTHENTICATION_BACKENDS = [
